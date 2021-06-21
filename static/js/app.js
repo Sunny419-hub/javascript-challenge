4f414f734dc1abff2df1390381bc641c8d78eab7
SoniaSu <frutos.rojos17@gmail.com>
2021-06-20 21:08:47 -0500

    update

--- static/js/app.js
+++ static/js/app.js
@@ -5,14 +5,14 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 // Console.log the data from data.js
-console.log(data);
+//console.log(data);
 
 // // Use d3 to update each cell's text with
 data.forEach(function(UFOinfo) {
-    console.log(UFOinfo);
+    //console.log(UFOinfo);
     var row = tbody.append("tr");
     Object.entries(UFOinfo).forEach(function([key, value]) {
-      console.log(key, value);
+      //console.log(key, value);
  //     // Append a cell to the row for each value
  //     // in the object
       var cell = row.append("td");
@@ -20,10 +20,9 @@ data.forEach(function(UFOinfo) {
     });
   });
  // Getting a reference to the button on the page with the id property set to `click-me`
-var button = d3.select("#click-me");
+var button = d3.select("#filter-btn");
 // We can use d3 to see the object that dispatched the event inline
 button.on("click", function() {
     console.log("Hi, a button was clicked!");
     console.log(d3.event.target);
   });
-  
