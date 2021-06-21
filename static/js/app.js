42269b940ebfe3002f413f070aff4ac0754d2bff
SoniaSu <frutos.rojos17@gmail.com>
2021-06-20 21:47:33 -0500

    update

--- static/js/app.js
+++ static/js/app.js
@@ -21,8 +21,28 @@ data.forEach(function(UFOinfo) {
   });
  // Getting a reference to the button on the page with the id property set to `click-me`
 var button = d3.select("#filter-btn");
+// Getting a reference to the input element on the page with the id property set to 'input-field'
+var input = d3.select("#datetime");
 // We can use d3 to see the object that dispatched the event inline
 button.on("click", function() {
-    console.log("Hi, a button was clicked!");
-    console.log(d3.event.target);
+    
+    tbody.html("");
+    var filteredata = data.filter(selectdatetime);
+    console.log(filteredata);
+    filteredata.forEach(function(UFOinfo) {
+        //console.log(UFOinfo);
+        var row = tbody.append("tr");
+        Object.entries(UFOinfo).forEach(function([key, value]) {
+          //console.log(key, value);
+     //     // Append a cell to the row for each value
+     //     // in the object
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
   });
+// Create a custom filtering function
+function selectdatetime(datetm) {
+    console.log(input.node().value);
+    return datetm.datetime == input.node().value;
+}
