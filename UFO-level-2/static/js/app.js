6acf37815bde44006ec39e1ab34f7fc0a397b7c1
SoniaSu <frutos.rojos17@gmail.com>
2021-06-20 22:43:48 -0500

    update

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -21,14 +21,20 @@ data.forEach(function(UFOinfo) {
   });
  // Getting a reference to the button on the page with the id property set to `click-me`
 var button = d3.select("#filter-btn");
-// Getting a reference to the input element on the page with the id property set to 'input-field'
-var input = d3.select("#datetime");
+
+// Getting a reference to the input element on the page with the id property set to 'input-field' Using multiple input tags to set multiple filters and search for UFO sightings
+var inputdatetm = d3.select("#datetime");
+var inputcity = d3.select("#city");
+var inputstate = d3.select("#state");
+var inputcountry = d3.select("#country");
+var inputshape = d3.select("#shape");
+
 // We can use d3 to see the object that dispatched the event inline
 button.on("click", function() {
     
     tbody.html("");
-    var filteredata = data.filter(selectdatetime);
-    //console.log(filteredata);
+    var filteredata = data.filter(selectvalues);
+        //console.log(filteredata);
     filteredata.forEach(function(UFOinfo) {
         //console.log(UFOinfo);
         var row = tbody.append("tr");
@@ -41,8 +47,12 @@ button.on("click", function() {
         });
       });
   });
-// Create a custom filtering function
-function selectdatetime(datetm) {
+
+// Function => Using multiple input tags to set multiple filters and search for UFO sightings
+
+// Create a date/time filtering function
+function selectdatetime(values) {
     //console.log(input.node().value);
-    return datetm.datetime == input.node().value;
+    return values.datetime == inputdatetm.node().value && values.city == inputcity.node().value && values.state == inputstate.node().value &&
+    values.country == inputcountry.node().value && values.shape == inputshape.node().value;
 }
